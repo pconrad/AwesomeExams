08a3b987e566eb1836aeff8d74b739578bf0ec59
Zachary Helfenstein <zhelfinstein@butthead.cs.ucsb.edu>
2014-04-16 16:24:01 -0700

    deleted vestigial code

--- js/questions/symbolicLogic.js
+++ js/questions/symbolicLogic.js
@@ -1,19 +1,6 @@
 //Parameter randomStream should be an instance of the RandomStream class.
 function symbolicLogicQuestion(randomStream)
 {
-    //Generate the list of variables for use
-    var A = "A";
-    var B = "B";
-    var C = "C";
-    this.values = [A,A,A,B,B,B,C,C,C];
-    randomStream.shuffle(values);
-    //this.a = {name: "A", value: values[0]};
-    //this.b = {name: "B", value: values[1]};
-    //this.c = {name: "C", value: values[2]};    
-
-    //Shuffle the operators
-    this.ops = [" and ", " or ", " only if ", " if and only if "];
-    randomStream.shuffle(this.ops);
 
     // --Begin Evin Edits--
     // Bool Operator classes, for doing quick generation and execution boolean expressions.
@@ -47,6 +34,7 @@ function symbolicLogicQuestion(randomStream)
 	}
 	//Todo: add a render function here
     }
+
     //Generate the question.
     //Needs a little improvement. Randomize left/right assignment. Maybe make it so it takes two exps
     //instead of one literal and one expressions.
@@ -61,42 +49,8 @@ function symbolicLogicQuestion(randomStream)
     // The kind of magical rendering energy that Walt Disney prepared you for
     // --End Evin Edits--
 
-    //The following statements decide what the symbolic logic should be in the given situation, based on the first two operators
-
-    //Calculate the correct answer and the distractor obtained by applying Order of Operations incorrectly
-    if(this.ops[0] == "+")
-    {
-        var correct = this.a + this.b*this.c;
-        var distract = (this.a+this.b)*this.c;
-    }
-    else
-    {
-        var correct = this.a * this.b + this.c;
-        var distract = this.a * (this.b + this.c);
-    }
-
-    //Pick two more distractors from within a range
-    var delta = distract - correct;
-    var lowerEnd = correct - delta ;
-    var upperEnd = distract + delta;
-    lowerEnd < 0 ? lowerEnd = 0 : lowerEnd = lowerEnd;
-
-    //Array of {int, bool} pairs, representing an answer option and whether or not it is the correct one
-    this.answerChoices = [ {value: correct, flag: true},
-                          {value: distract, flag: false},
-                          {value: randomStream.nextIntRange(upperEnd-lowerEnd) + lowerEnd, flag: false},
-                          {value: randomStream.nextIntRange(upperEnd-lowerEnd) + lowerEnd, flag: false} ];
-
-    randomStream.shuffle(this.answerChoices);
-
-    //Find the correct answer
-    this.correctIndex = 0;
-    for(var i=0; i<this.answerChoices.length; i++)
-    {
-        if(this.answerChoices[i].flag == true)
-            this.correctIndex = i;           
-    }
     
+        
     this.formatQuestion = function(format) {
       switch (format) {
          case "HTML": return this.formatQuestionHTML();
