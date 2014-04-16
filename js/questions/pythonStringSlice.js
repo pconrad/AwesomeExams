e2b1db1a35cb3f82aa7c097c523d394c6ee7fd46
Jeremy Irvin <jirvin@csil.cs.ucsb.edu>
2014-04-15 18:55:35 -0700

    added error question, added quotes to answers

--- js/questions/pythonStringSlice.js
+++ js/questions/pythonStringSlice.js
@@ -8,12 +8,9 @@ function pythonStringSliceQuestion(randomStream) {
     this.index1 = randomStream.nextIntRange(this.index2 + 1);//if index1=0, print nothing
     this.index = 0;
     if (this.index2 === this.index1) {
-        if(this.index1 === this.name.length) {
-            this.index = this.index1-1;
-        } else {
-            this.index = this.index2;
-        }
+            this.index=this.index2;
     }
+
 //Array of {String, bool} pairs: string and a flag indicating whether or not it is the correct answer.
     this.answerChoices = new Array(4);
     if (this.index === 0) {
@@ -25,18 +22,18 @@ function pythonStringSliceQuestion(randomStream) {
         } else if (this.index1 === 0 && this.index2 === 1) {
             this.answerChoices[0] = {value: this.name[0], flag: true};
             this.answerChoices[1] = {value: this.name[1], flag: false};
-            this.answerChoices[2] = {value: "\" \"", flag: false};//quotes or empty space?
+            this.answerChoices[2] = {value: " ", flag: false};//quotes or empty space?
             this.answerChoices[3] = {value: this.name.substring(0, 2), flag: false};
         } else if (this.index1 === this.name.length-1 && this.index2 === this.name.length) {
             this.answerChoices[0] = {value: this.name[this.name.length - 1], flag: true};
             this.answerChoices[1] = {value: this.name.substring(this.name.length - 2, this.name.length), flag: false}; 
-            this.answerChoices[2] = {value: "\" \"", flag: false};             
+            this.answerChoices[2] = {value: " ", flag: false};             
             this.answerChoices[3] = {value: this.name[this.name.length - 2], flag: false};
         } else if (this.index1+1 === this.index2) {
             this.answerChoices[0] = {value: this.name.substring(this.index1, this.index2), flag: true};
             this.answerChoices[1] = {value: this.name.substring(this.index1, this.index2 + 1), flag: false};
             this.answerChoices[2] = {value: this.name.substring(this.index1 - 1, this.index2), flag: false};
-            this.answerChoices[3] = {value: "\" \"", flag: false};
+            this.answerChoices[3] = {value: " ", flag: false};
         } else if (this.index1 === 0) {
             this.answerChoices[0] = {value: this.name.substring(0, this.index2), flag: true};
             this.answerChoices[1] = {value: this.name.substring(1, this.index2 + 1), flag: false};
@@ -54,16 +51,22 @@ function pythonStringSliceQuestion(randomStream) {
             this.answerChoices[3] = {value: this.name.substring(this.index1 - 1, this.index2 + 1), flag: false};
         } 
     } else {
+        if(this.index === this.name.length) {
+            this.answerChoices[0]= {value: "IndexError: string index out of range", flag: true};
+            this.answerChoices[1]= {value: this.name[this.index-1], flag: false};
+            this.answerChoices[2]= {value: " ", flag: false};
+            this.answerChoices[3]= {value: this.name[0], flag: false};
+        }
         if (this.index === 0) {
             this.answerChoices[0] = {value: this.name[0], flag: true};
             this.answerChoices[1] = {value: this.name[1], flag: false};
             this.answerChoices[2] = {value: this.name.substring(0, 2), flag: false};
-            this.answerChoices[3] = {value: "\" \"", flag: false};
+            this.answerChoices[3] = {value: " ", flag: false};
         } else if (this.index === this.name.length - 1) {
             this.answerChoices[0] = {value: this.name[this.name.length - 1], flag: true};
             this.answerChoices[1] = {value: this.name[this.name.length - 2], flag: false};
             this.answerChoices[2] = {value: this.name.substring(this.name.length - 2, this.name.length), flag: false};
-            this.answerChoices[3] = {value: "\" \"", flag: false};
+            this.answerChoices[3] = {value: " ", flag: false};
         } else {
             this.answerChoices[0] = {value: this.name[this.index], flag: true};
             this.answerChoices[1] = {value: this.name[this.index - 1], flag: false};
@@ -106,13 +109,26 @@ function pythonStringSliceQuestion(randomStream) {
             questionText += "[" + this.index + "]?";
         }
             questionText += "<br>";
-    
+        var x=["a", "b", "c", "d"];
+        if(this.index1 === this.name.length) {
+            questionText+="<p>";
+            for(i=0;i<4;i++) {
+                if(this.answerChoices[i].flag==true) {
+                    questionText+="<br><strong>"+x[i]+") </strong>" + this.answerChoices[i].value;
+                }    
+                else {
+                    questionText+="<br><strong>" +x[i]+ ") </strong>"+"\'" + this.answerChoices[i].value + "\'";
+                }
+             }
+             questionText+="</p>";
+        }
+        else {
             questionText += "<p><strong>a) </strong>" 
-                + this.answerChoices[0].value + "<br><strong>b) </strong>"
-                + this.answerChoices[1].value + "<br><strong>c) </strong>"
-                + this.answerChoices[2].value + "<br><strong>d) </strong>"
-                + this.answerChoices[3].value + "</p>";
-
+                + "\'" + this.answerChoices[0].value +"\'" + "<br><strong>b) </strong>"
+                + "\'" + this.answerChoices[1].value +"\'" +  "<br><strong>c) </strong>"
+                + "\'" + this.answerChoices[2].value +"\'" +  "<br><strong>d) </strong>"
+                + "\'" + this.answerChoices[3].value +"\'" + "</p>";
+        }
         return questionText;
     };
 
