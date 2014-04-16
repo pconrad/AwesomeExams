abcee0d8d6e653fbc3777f3143509478f28b9d18
Evin Sellin <evin@csil.cs.ucsb.edu>
2014-04-16 01:46:37 -0700

    Added BoolExp classes in the Logic Question function

--- js/questions/symbolicLogic.js
+++ js/questions/symbolicLogic.js
@@ -15,9 +15,53 @@ function symbolicLogicQuestion(randomStream)
     this.ops = [" and ", " or ", " only if ", " if and only if "];
     randomStream.shuffle(this.ops);
 
+    // --Begin Evin Edits--
+    // Bool Operator classes, for doing quick generation and execution boolean expressions.
+    // Vector is associative array of bool values to be tested (used for building answer key)
+    
+    var literalList = ["A", "B", "C"];
+    var depth = 4;//Number of operators in a question
+    function Literal( name ){
+	this.name = name;
+	this.exec = function( vector ){
+	    return vector[this.name];
+	}
+	//Todo: add a render function here.
+    }
+    
+    function BoolExpr( a, b, op ){
+	this.a = a;
+	this.b = b;
+	this.op = op;
+	this.exec = function( vector ){
+	    //Go down the tree of subexpressions
+	    var aVal = this.a.exec( vector );
+	    var bVal = this.b.exec( vector );
+	    switch( this.op ){
+		//We really should have a better method of encoding these operations.
+		" and ": return aVal && bVal;
+		" or ": return aVal || bVal;
+		" if and only if ": return (aVal&&bVal) || ((!aVal)&&(!bVal));
+		" only if ": return aVal?bVal:true;
+	    }
+	}
+	//Todo: add a render function here
+    }
+    //Generate the question.
+    //Needs a little improvement. Randomize left/right assignment. Maybe make it so it takes two exps
+    //instead of one literal and one expressions.
+    var lit = literalList[randomStream.next()%(literalList.length-1)];
+    var exp = new Literal(literalList[initLit]);
+    for( var i=0; i<depth; var++ ){
+	lit = literalList[randomStream.next()%(literalList.length-1)];
+	op = this.ops[randomStream.next()%(this.ops.length-1)];
+	exp = new BoolExpr( lit, op, exp );
+    }
+    // With a simple exp.render() you can make everything come to life
+    // The kind of magical rendering energy that Walt Disney prepared you for
+    // --End Evin Edits--
 
     //The following statements decide what the symbolic logic should be in the given situation, based on the first two operators
-    
 
     //Calculate the correct answer and the distractor obtained by applying Order of Operations incorrectly
     if(this.ops[0] == "+")
