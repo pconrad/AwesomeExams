10f76abd40f8952a115867f3b63ae28ab7fe4a40
Evan Crook <ecrook@umail.ucsb.edu>
2014-05-08 17:22:44 -0700

    First working version of a JSON system for generating quizzes

---
+++ js/parser.js
@@ -0,0 +1,60 @@
+//A parser for the JSON quiz notation
+
+//Takes an array of question-generating objects
+//Returns an array of questions
+function parseQuizArray(quizArray, randomStream)
+{
+	var questions = [];
+	
+	//stolen from quiz.js
+	//Needs to have "included" (aka called in the same html file) all the specific question-type js files
+	var questionFunctions = {
+	"changeOfBase":          {"f": changeOfBaseQuestion,       title: "Change of Base"},
+	"orderOfOperations":     {"f": orderOfOperationsQuestion,  title: "Order of Operations"},
+	"operandsAndOperators":  {"f":operandsAndOperatorsQuestion,title: "Operands and Operators"},
+	"pythonProgramOutput":   {"f":pythonProgramOutputQuestion, title: "Python Program Output"},
+	"pythonStringSlice":    {"f":pythonStringSliceQuestion,    title: "Python String Slice"},
+	"symbolicLogic":         {"f":symbolicLogicQuestion,       title: "Symbolic Logic"},
+	"CvariableType":         {"f":CvariableTypeQuestion,       title: "C Variable Type"},
+	"cStrings":              {"f":cStringsQuestion,            title: "C Strings"},
+	"pyStrings":             {"f":pyStringsQuestion,           title: "Python Strings"}
+    };
+	
+	//Loop through the array of generating objects
+	for(var i = 0; i < quizArray.length; ++i)
+	{
+		var item = quizArray[i];
+		
+		if("question" in item) //If this object is a question generator
+		{
+			var questionType = item.question;
+			var parameters = (("parameters" in item) ? item.parameters : {});
+			var repeat = (("repeat" in item) ? item.repeat : 1);
+			
+			var questionFunc = ((questionType in questionFunctions) ? questionFunctions[questionType].f : null);
+			
+			//Generate the specified number of the specified type of question, add them to the array
+			if(questionFunc != null)
+			{
+				for(var j=0; j<repeat; j++)
+				{
+					questions.push(new questionFunc(randomStream)); //once parameters are implemented, this call will be questionFunc(randomStream, parameters)
+				}
+			}
+		}
+		
+		if("chooseK" in item)
+		{
+			var k = item.chooseK;
+			var itemsArray = (("items" in item) ? item.items : []);
+			var newArray = parseQuizArray(itemsArray, randomStream); //Recursively parse the array passed to chooseK, which can contain question generators or more chooseK objects
+			//After this finishes newArray should consist solely of question objects
+			randomStream.shuffle(newArray);
+			questions = questions.concat(newArray.slice(0,k)); //Grab the first k elements and add them to the questions array
+		}
+		
+		//More object types to be added here as they are implemented: shuffle, etc...
+	}
+	
+	return questions; 
+}
