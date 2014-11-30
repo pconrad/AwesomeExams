70ae150180bb19efc63ea3a87ebc47fc15125d95
Phill Conrad <pconrad@cs.ucsb.edu>
2014-11-30 09:22:14 -0800

    First version of awesome java project produced by Maven ... just scaffolding at this point

---
+++ java/awesome/README.md
@@ -0,0 +1,5 @@
+To execute the Hello World program produced by the Maven setup script, do:
+
+```bash
+ java -cp target/awesome-1.0-SNAPSHOT.jar edu.ucsb.cs.pconrad.awesome.App
+```
---
+++ java/awesome/src/main/java/edu/ucsb/cs/pconrad/awesome/App.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs.pconrad.awesome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
