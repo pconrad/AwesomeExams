e45bf9ccefeaf69a720c31e24e7e5690d01afcd9
Jasen Hall <jasen@cs.ucsb.edu>
2015-01-30 06:20:06 -0800

    Started QUnit setup.

--- js/questions/cppUtilities.js
+++ js/questions/cppUtilities.js
@@ -29,24 +29,5 @@ function cppGenerateRandomValue(randomStream, num)  // 0: integer, 1: double, 2:
 
 function cppGetRandomId(randomStream, num)
 {
-/*    var id;
-
-    switch(num){
-        case 0:
-            id = CppRandomNames.one[randomStream.nextIntRange(CppRandomNames.one.length)];
-            break;
-        case 1:
-            id = CppRandomNames.two[randomStream.nextIntRange(CppRandomNames.two.length)];
-            break;
-        case 2:
-            id = CppRandomNames.three[randomStream.nextIntRange(CppRandomNames.three.length)];
-            break;
-        case 3:
-            id = CppRandomNames.four[randomStream.nextIntRange(CppRandomNames.four.length)];
-            break;
-        default:
-            break;
-    }
-*/
     return CppRandomNames[num][randomStream.nextIntRange(CppRandomNames[num].length)];
 }
