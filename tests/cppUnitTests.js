21bab596c89e165d1bcde856e833079af1a67398
Jasen Hall <jasen@cs.ucsb.edu>
2015-01-30 13:49:14 -0800

    Having trouble reaching code in the unit tests.

--- tests/cppUnitTests.js
+++ tests/cppUnitTests.js
@@ -1,3 +1,58 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+QUnit.module("cppUtilities");
+QUnit.test("getRandomReturnType returns an expected value", function(assert) {
+  var rs = RandomStream(determineSeed("0x0"));
+  assert.ok(rs != undefined, "able to instance RandomStream");
+  assert.equal(rs.nextIntRange(2), -1, "RandomStream works in test");
+  var ret = getRandomReturnType(rs);
+
+  assert.ok(ret === "int" || ret === "float" || ret === "double" ||
+    ret === "string", "returned expected return type");
+});
+
+QUnit.module("cppAppropriateVariabes");
+QUnit.test("test", function(assert) {
+  var rs = RandomStream(determineSeed("0x0"));
+  var q = cppAppropriateVariablesQuestion(rs);
+
+  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+});
+
+QUnit.module("cppArgcArgv");
+QUnit.test("test", function(assert) {
+  var rs = RandomStream(determineSeed("0x0"));
+  var q = cppArgcArgvQuestion(rs);
+
+  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+});
+
+QUnit.module("cppBooleanEval");
+QUnit.test("test", function(assert) {
+  var rs = RandomStream(determineSeed("0x0"));
+  var q = cppBooleanEval(rs);
+
+  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+});
+
+QUnit.module("cppFunctionOverloading");
+QUnit.test("test", function(assert) {
+  var rs = RandomStream(determineSeed("0x0"));
+  var q = cppFunctionOverloading(rs);
+
+  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+});
+
+QUnit.module("cppFunctionParameters");
+QUnit.test("test", function(assert) {
+  var rs = RandomStream(determineSeed("0x0"));
+  var q = cppFunctionParameters(rs);
+
+  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+});
+
+QUnit.module("cppPointerAssignment");
+QUnit.test("test", function(assert) {
+  var rs = RandomStream(determineSeed("0x0"));
+  var q = cppPointerAssignment(rs);
+
+  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
 });
