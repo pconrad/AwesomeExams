b36cb9b3abae28cf4f734c6a2e4eda97d0618481
Jasen Hall <jasen@cs.ucsb.edu>
2015-02-13 13:30:51 -0800

    Tests working now. Thanks @pconrad!

--- test/cppUnitTests.js
+++ test/cppUnitTests.js
@@ -1,9 +1,18 @@
-var rs = RandomStream(determineSeed("0x0"));
+//var rs = RandomStream(determineSeed("0x0"));
 
 //QUnit.module("cppUtilities");
+
+QUnit.module( "cppUtilities", {
+  setup: function( assert ) {
+  }, teardown: function( assert ) {
+    assert.ok( true, "teardown successful" );
+  }
+});
+
 QUnit.test("getRandomReturnType returns an expected value", function(assert) {
+  var rs = new RandomStream(determineSeed("0x0"));
   assert.ok(rs != undefined, "able to instance RandomStream");
-  assert.equal(rs.nextIntRange(2), -1, "RandomStream works in test");
+  assert.equal(rs.nextIntRange(2), 1, "RandomStream works in test");
   var ret = getRandomReturnType(rs);
 
   assert.ok(ret === "int" || ret === "float" || ret === "double" ||
@@ -11,49 +20,59 @@ QUnit.test("getRandomReturnType returns an expected value", function(assert) {
 });
 
 //QUnit.module("cppAppropriateVariabes");
-QUnit.test("test", function(assert) {
-  var rs = RandomStream(determineSeed("0x0"));
-  var q = cppAppropriateVariablesQuestion(rs);
+QUnit.test("cppAppropriateVariablesQuestion returns expected question", function(assert) {
+  var rs = new RandomStream(determineSeed("0x0"));
+  assert.ok(rs != undefined, "able to instance RandomStream");
+  //var q = cppAppropriateVariablesQuestion(rs);
+  var q = new CppApproVar(rs);
 
-  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+  assert.ok(q != undefined, "instanced questions" + JSON.stringify(q));
+  //var result = q.foo();
+  //assert.equal(result, 1, "Test simple function " + result);
+  assert.equal(q.formatQuestionHTML(), "", "quiz is correct");
 });
 
 //QUnit.module("cppArgcArgv");
 QUnit.test("test", function(assert) {
-  var rs = RandomStream(determineSeed("0x0"));
+  var rs = new RandomStream(determineSeed("0x0"));
+  assert.ok(rs != undefined, "able to instance RandomStream");
   var q = cppArgcArgvQuestion(rs);
 
-  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+  assert.equal(q.formatQuestionsHTML, "", "quiz is correct");
 });
 
 //QUnit.module("cppBooleanEval");
 QUnit.test("test", function(assert) {
-  var rs = RandomStream(determineSeed("0x0"));
+  var rs = new RandomStream(determineSeed("0x0"));
+  assert.ok(rs != undefined, "able to instance RandomStream");
   var q = cppBooleanEval(rs);
 
-  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+  assert.equal(q.formatQuestionsHTML, "", "quiz is correct");
 });
 
 //QUnit.module("cppFunctionOverloading");
 QUnit.test("test", function(assert) {
-  var rs = RandomStream(determineSeed("0x0"));
+  var rs = new RandomStream(determineSeed("0x0"));
+  assert.ok(rs != undefined, "able to instance RandomStream");
   var q = cppFunctionOverloading(rs);
 
-  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+  assert.equal(q.formatQuestionsHTML, "", "quiz is correct");
 });
 
 //QUnit.module("cppFunctionParameters");
 QUnit.test("test", function(assert) {
-  var rs = RandomStream(determineSeed("0x0"));
+  var rs = new RandomStream(determineSeed("0x0"));
+  assert.ok(rs != undefined, "able to instance RandomStream");
   var q = cppFunctionParameters(rs);
 
-  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+  assert.equal(q.formatQuestionsHTML, "", "quiz is correct");
 });
 
 //QUnit.module("cppPointerAssignment");
 QUnit.test("test", function(assert) {
-  var rs = RandomStream(determineSeed("0x0"));
+  var rs = new RandomStream(determineSeed("0x0"));
+  assert.ok(rs != undefined, "able to instance RandomStream");
   var q = cppPointerAssignment(rs);
 
-  assert.equals(q.formatQuestionsHTML, "", "quiz is correct");
+  assert.equal(q.formatQuestionsHTML, "", "quiz is correct");
 });
