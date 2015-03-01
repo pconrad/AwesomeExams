4be7434a90038851c1935e4891789d1b86a4f506
Phill Conrad <pconrad@cs.ucsb.edu>
2015-02-28 16:12:59 -0800

    jquery

---
+++ jquery/jquery.shuffle.js
@@ -0,0 +1,44 @@
+/*
+ * jQuery shuffle
+ *
+ * Copyright (c) 2008 Ca-Phun Ung <caphun at yelotofu dot com>
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * http://yelotofu.com/labs/jquery/snippets/shuffle/
+ *
+ * Shuffles an array or the children of a element container.
+ * This uses the Fisher-Yates shuffle algorithm <http://jsfromhell.com/array/shuffle [v1.0]>
+ */
+ 
+(function($){
+
+$.fn.selectOne = function() {
+		return this.each(function(){
+			var items = $(this).children().clone(true);
+			return (items.length) ? $(this).html($.selectOne(items)) : this;
+		});
+	}
+	
+	$.selectOne = function(arr) {
+		
+		for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+		this(arr[0]).show();
+		for (var k= 1; k<=arr.length; k++)
+     		this(arr[k]).hide();
+		return arr;
+	}
+
+	$.fn.shuffle = function() {
+		return this.each(function(){
+			var items = $(this).children().clone(true);
+			return (items.length) ? $(this).html($.shuffle(items)) : this;
+		});
+	}
+	
+	$.shuffle = function(arr) {
+		for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+		return arr;
+	}
+	
+})(jQuery);
